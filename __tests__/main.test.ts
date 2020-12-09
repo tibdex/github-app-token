6bada9eb186b7ef7f87511c08364ad0913f97db8
adoami <adoami@chanzuckerberg.com>
2020-12-09 12:22:11 -0800

    added testing capabilities to getting the token

---
+++ __tests__/main.test.ts
@@ -0,0 +1,11 @@
+import { getToken } from '../src/get_token'
+
+// TODO: Parameters for getToken include the private key from the Github ok-to-test App,
+// the app id that can be found in the Github App UI, the owner of the repo, and the repo
+// name where the app is installed
+// test('generate token', async () => {
+//   const input = await getToken(
+//     ``, "", { owner: "", repo: "" })
+//   // https://jestjs.io/docs/en/expect
+//   console.log(input)
+// })
---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  clearMocks: true,
+  moduleFileExtensions: ['js', 'ts'],
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  preset: 'ts-jest',
+  verbose: true
+}
