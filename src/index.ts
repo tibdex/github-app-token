d196e9580c96765a13bf95d632d678789d2a984a
Thibault Derousseaux <6574550+tibdex@users.noreply.github.com>
2020-12-20 19:11:23 +0100

    Use @octokit/auth-app (#15)
    Duplicate of #14 to make some adjustments.
    
    Co-authored-by: adoami <adoami@chanzuckerberg.com>
    Co-authored-by: edulop <elopez@chanzuckerberg.com>

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,31 @@
-import {
-  error as logError,
-  getInput,
-  info,
-  setFailed,
-  setOutput,
-  setSecret,
-} from "@actions/core";
-import { context, getOctokit } from "@actions/github";
-import { App } from "@octokit/app";
+import { getInput, info, setFailed, setOutput, setSecret } from "@actions/core";
+
 import isBase64 from "is-base64";
+
+import { fetchInstallationToken } from "./fetch-installation-token";
 
 const run = async () => {
   try {
-    const id = Number(getInput("app_id", { required: true }));
+    const appId = getInput("app_id", { required: true });
     const privateKeyInput = getInput("private_key", { required: true });
     const privateKey = isBase64(privateKeyInput)
       ? Buffer.from(privateKeyInput, "base64").toString("utf8")
       : privateKeyInput;
-    const app = new App({ id, privateKey });
-    const jwt = app.getSignedJsonWebToken();
-    const octokit = getOctokit(jwt);
-    const {
-      data: { id: installationId },
-    } = await octokit.apps.getRepoInstallation(context.repo);
-    const token = await app.getInstallationAccessToken({
-      installationId,
+
+    const installationToken = await fetchInstallationToken({
+      appId,
+      privateKey,
     });
-    setSecret(token);
-    setOutput("token", token);
+
+    setSecret(installationToken);
+    setOutput("token", installationToken);
     info("Token generated successfully!");
   } catch (error: unknown) {
-    if (typeof error !== "string" && !(error instanceof Error)) {
-      throw new TypeError(`Caught error of unexpected type: ${typeof error}`);
+    if (typeof error === "string" || error instanceof Error) {
+      setFailed(error);
+    } else {
+      setFailed(`Caught error of unexpected type: ${typeof error}`);
     }
-
-    setFailed(error);
   }
 };
 
