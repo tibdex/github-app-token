516ef6015c38a575a3cc87e8840c79350e0238e9
Min Huang <57320+retiman@users.noreply.github.com>
2021-08-19 01:46:43 -0700

    Add support for GitHub Enterprise (#24)

--- src/fetch-installation-token.ts
+++ src/fetch-installation-token.ts
@@ -1,5 +1,6 @@
-import { context, getOctokit } from "@actions/github";
+import { getOctokit } from "@actions/github";
 import { createAppAuth } from "@octokit/auth-app";
+import { request } from "@octokit/request";
 
 export const fetchInstallationToken = async ({
   appId,
@@ -12,7 +13,15 @@ export const fetchInstallationToken = async ({
   privateKey: string;
   repo: string;
 }>): Promise<string> => {
-  const app = createAppAuth({ appId, privateKey });
+  const app = createAppAuth({
+    appId,
+    privateKey,
+    request: request.defaults({
+      // GITHUB_API_URL is part of GitHub Actions' built-in environment variables.
+      // See https://docs.github.com/en/actions/reference/environment-variables#default-environment-variables.
+      baseUrl: process.env["GITHUB_API_URL"]
+    })
+  });
   const authApp = await app({ type: "app" });
   const octokit = getOctokit(authApp.token);
   const {
