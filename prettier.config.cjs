bdb18417fcf4e4297678634810e3f840815ced30
Thibault Derousseaux <tibdex@gmail.com>
2023-01-26 11:36:55 -0500

    Upgrade deps

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,9 @@
 "use strict";
 
 module.exports = {
+  importOrder: ["^node:(.*)$", "<THIRD_PARTY_MODULES>", "^[./]"],
+  importOrderGroupNamespaceSpecifiers: true,
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
   trailingComma: "all",
 };
