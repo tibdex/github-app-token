e91e1a085778aa4003a791bff496a9fc3631eb03
Thibault Derousseaux <tibdex@gmail.com>
2023-09-19 11:59:54 -0400

    fixup! Add option to not revoke token

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,7 @@
-import { info, saveState, setOutput, setSecret } from "@actions/core";
 
-import { createInstallationAccessToken } from "./create-installation-access-token.js";
-import { parseOptions } from "./parse-options.js";
 import { run } from "./run.js";
-import { tokenKey } from "./state.js";
+import { getInput } from "@actions/core";
 
 await run(async () => {
-  const options = parseOptions();
-  const token = await createInstallationAccessToken(options);
-  setSecret(token);
-  saveState(tokenKey, token);
-  setOutput("token", token);
-  info("Token created successfully");
+  console.log(JSON.stringify({value: getInput("revoke")}));
 });
